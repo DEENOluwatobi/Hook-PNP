5d524842494a8a0a7f0e8b797d0f24eb61829a12
DEENOluwatobi <shodimutobie@gmail.com>
2023-07-21 17:35:36 +0100

    fixed onclick events

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const loginScn = document.getElementById("l-scn");
 const signUpScn = document.getElementById("s-scn");
 
 const PopUpScreen = () => {    
-  if (loginScn.style.display === "none") {
-    loginScn.style.display = "flex";
-    menu.style.display = "none";
-  } else {
+  if (loginScn.style.display === "flex") {
     loginScn.style.display = "none";
+    menu.style.display = "flex";
+  } else {
+    loginScn.style.display = "flex";
   }
 };
 
 const SignUpScreen = () => {    
-  if (signUpScn.style.display === "none") {
-    signUpScn.style.display = "flex";
-    menu.style.display = "none";
-  } else {
+  if (signUpScn.style.display === "flex") {
     signUpScn.style.display = "none";
+    menu.style.display = "flex";
+  } else {
+    signUpScn.style.display = "flex";
   }
 };
 
@@ -26,10 +26,10 @@ const ClosePopUpScreen = () => {
 };
 
 const toggleMenu = () => {    
-    if (menu.style.display === "none") {
-      menu.style.display = "flex";
-    } else {
+    if (menu.style.display === "flex") {
       menu.style.display = "none";
+    } else {
+      menu.style.display = "flex";
     }
 };
 
