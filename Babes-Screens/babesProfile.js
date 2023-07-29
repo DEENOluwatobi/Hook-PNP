7553cd5833b70f9915d2ac06482e5b4e7f153bb9
DEENOluwatobi <shodimutobie@gmail.com>
2023-07-29 10:49:44 +0100

    drag and drop component fixed

--- Babes-Screens/babesProfile.js
+++ Babes-Screens/babesProfile.js
@@ -1,61 +1,63 @@
-const dropArea = document.getElementById('dropArea');
-let isFileInputCreated = false; // Flag to track if file input is already created
+const dropAreas = document.querySelectorAll('.dropArea');
+  let isFileInputCreated = false; // Flag to track if file input is already created
 
-dropArea.addEventListener('click', () => {
-  if (!isFileInputCreated) {
-    isFileInputCreated = true; // Set the flag to true to prevent further creation
+  dropAreas.forEach(dropArea => {
+    dropArea.addEventListener('click', () => {
+      if (!isFileInputCreated) {
+        isFileInputCreated = true; // Set the flag to true to prevent further creation
 
-    const fileInput = document.createElement('input');
-    fileInput.type = 'file';
-    fileInput.accept = 'image/*';
-    fileInput.style.display = 'none';
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.accept = 'image/*';
+        fileInput.style.display = 'none';
 
-    fileInput.addEventListener('change', () => {
-      const file = fileInput.files[0];
-      handleFile(file);
-      isFileInputCreated = false; // Reset the flag when file is selected
+        fileInput.addEventListener('change', () => {
+          const file = fileInput.files[0];
+          handleFile(file, dropArea); // Pass dropArea to handleFile function
+          isFileInputCreated = false; // Reset the flag when file is selected
+        });
+
+        dropArea.appendChild(fileInput);
+        fileInput.click();
+        dropArea.removeChild(fileInput);
+      }
     });
 
-    dropArea.appendChild(fileInput);
-    fileInput.click();
-    dropArea.removeChild(fileInput);
+    dropArea.addEventListener('dragover', (e) => {
+      e.preventDefault();
+      dropArea.classList.add('highlight');
+    });
+
+    dropArea.addEventListener('dragleave', () => {
+      dropArea.classList.remove('highlight');
+    });
+
+    dropArea.addEventListener('drop', (e) => {
+      e.preventDefault();
+      dropArea.classList.remove('highlight');
+      const file = e.dataTransfer.files[0];
+      handleFile(file, dropArea); // Pass dropArea to handleFile function
+    });
+  });
+
+  function handleFile(file, dropArea) {
+    if (!file) return;
+
+    // Check if the file is an image
+    if (file.type.startsWith('image/')) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const image = new Image();
+        image.src = e.target.result;
+
+        // You can display the image or perform further actions here
+        // For example, create a new image element and append it to the drop area:
+        const newImageElement = document.createElement('img');
+        newImageElement.src = e.target.result;
+        dropArea.appendChild(newImageElement);
+      };
+
+      reader.readAsDataURL(file);
+    }
   }
-});
-
-dropArea.addEventListener('dragover', (e) => {
-  e.preventDefault();
-  dropArea.classList.add('highlight');
-});
-
-dropArea.addEventListener('dragleave', () => {
-  dropArea.classList.remove('highlight');
-});
-
-dropArea.addEventListener('drop', (e) => {
-  e.preventDefault();
-  dropArea.classList.remove('highlight');
-  const file = e.dataTransfer.files[0];
-  handleFile(file);
-});
-
-function handleFile(file) {
-  if (!file) return;
-
-  // Check if the file is an image
-  if (file.type.startsWith('image/')) {
-    const reader = new FileReader();
-
-    reader.onload = (e) => {
-      const image = new Image();
-      image.src = e.target.result;
-
-      // You can display the image or perform further actions here
-      // For example, create a new image element and append it to the drop area:
-      const newImageElement = document.createElement('img');
-      newImageElement.src = e.target.result;
-      dropArea.appendChild(newImageElement);
-    };
-
-    reader.readAsDataURL(file);
-  }
-}
