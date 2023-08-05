8b2b6c80d01ae0efc81411eab3f4da17eb2e77bc
DEENOluwatobi <shodimutobie@gmail.com>
2023-08-05 13:46:27 +0100

    first page for search

--- search.js
+++ search.js
@@ -1,83 +1,200 @@
-// Sample array of data
+// HANDLES EVERYTHING ABOUT SEARCH
+
+
+const SlideUp = () => {
+  const NavBar = document.getElementById('navBar')
+  const welcomeDiv = document.getElementById('welcomeDiv');
+  const searchConDiv = document.getElementById('searchConDiv');
+  const searchHead = document.getElementById('search-head')
+  
+  NavBar.style.display = 'none'
+  welcomeDiv.style.opacity = '0';
+  welcomeDiv.style.height = '0';
+  welcomeDiv.style.transition = 'opacity 0.5s, height 0.5s';
+  searchHead.style.display = 'flex'
+  searchHead.style.opacity = '1'
+  searchHead.style.transition = 'opacity 0.5s, height 0.5s';
+
+  NavBar.style.transition = 'display 0.5s'
+
+  searchHead.style.marginTop = '10px'; 
+  searchHead.style.transition = 'margin-top 0.5s';
+  
+  searchConDiv.style.marginTop = '50px'; 
+  searchConDiv.style.transition = 'margin-top 0.5s';  
+}
+
+// ----------------------------------------------------------------------------
+
 const database = [
-  { picture: "image1.jpg", name: "John Doe", online: true, country: "USA" },
-  { picture: "image2.jpg", name: "Jane Smith", online: false, country: "Canada" },
-  // Add more data objects as needed
+  {
+      picture: 'Images/lady1.jpeg',
+      name: 'Kim Wesley',
+      state: 'New York',
+      country: 'USA',
+      online: true,
+  },
+  {
+      picture: 'Images/lady2.jpeg',
+      name: 'Mama Debby',
+      state: 'Lagos',
+      country: 'Nigeia',
+      online: true,
+  },
+  {
+      picture: 'Images/lady3.jpeg',
+      name: 'Juliet Sexy',
+      state: 'Tokyo',
+      country: 'China',
+      online: false,
+  },
+  {
+      picture: 'Images/lady4.jpeg',
+      name: 'Jade Gomez',
+      state: 'Lagos',
+      country: 'Nigeria',
+      online: false,
+  },
+  {
+      picture: 'Images/lady5.jpeg',
+      name: 'Mama Debby',
+      state: 'Ogun',
+      country: 'Nigeria',
+      online: true
+  },
+
+  {
+      picture: 'Images/lady1.jpeg',
+      name: 'Kim Wesley',
+      state: 'New York',
+      country: 'USA',
+      online: true,
+  },
+  {
+      picture: 'Images/lady2.jpeg',
+      name: 'Mama Debby',
+      state: 'Lagos',
+      country: 'Nigeia',
+      online: true,
+  },
+  {
+      picture: 'Images/lady3.jpeg',
+      name: 'Juliet Sexy',
+      state: 'Tokyo',
+      country: 'China',
+      online: false,
+  },
+  {
+      picture: 'Images/lady4.jpeg',
+      name: 'Jade Gomez',
+      state: 'Lagos',
+      country: 'Nigeria',
+      online: false,
+  },
+  {
+      picture: 'Images/lady5.jpeg',
+      name: 'Mama Debby',
+      state: 'Ogun',
+      country: 'Nigeria',
+      online: true
+  },
+  {
+    picture: 'Images/lady4.jpeg',
+    name: 'Jade Gomez',
+    state: 'Lagos',
+    country: 'Nigeria',
+    online: false,
+},
+{
+    picture: 'Images/lady5.jpeg',
+    name: 'Mama Debby',
+    state: 'Ogun',
+    country: 'Nigeria',
+    online: true
+},
 ];
-
-// const searchInput = document.getElementById("searchInput");
-// const resultsContainer = document.getElementById("resultsContainer");
-
-// const displayResults = (results) => {
-//   // Clear previous results
-//   resultsContainer.innerHTML = "";
-
-//   // Display new results
-//   results.forEach((result) => {
-//     const resultElement = document.createElement("div");
-//     resultElement.innerHTML = `
-//       <img src="${result.picture}" alt="${result.name}">
-//       <h3>${result.name}</h3>
-//       <p>${result.online ? "Online" : "Offline"}</p>
-//       <p>${result.country}</p>
-//     `;
-//     resultsContainer.appendChild(resultElement);
-//   });
-// };
-
-// const handleSearch = (event) => {
-//   event.preventDefault(); // Prevent form submission
-
-//   const query = searchInput.value.trim().toLowerCase();
-//   const results = database.filter((item) =>
-//     item.country.toLowerCase().includes(query)
-//   );
-
-//   displayResults(results);
-//   alert("Yo");
-// };
-
-// document.querySelector(".search-form").addEventListener("submit", handleSearch);
-
 
 const searchInput = document.getElementById("searchInput");
 const resultsContainer = document.getElementById("resultsContainer");
 
 const displayResults = (results) => {
-  resultsContainer.innerHTML = ''; // Clear previous results
-
-  if (results.length === 0) {
-    resultsContainer.innerHTML = '<p>No results found</p>';
-    return;
-  }
+  resultsContainer.innerHTML = "";
 
   results.forEach((result) => {
-    const resultElement = document.createElement("div");
-    resultElement.innerHTML = `
-      <img src="${result.picture}" alt="${result.name}">
-      <h3>${result.name}</h3>
-      <p>${result.online ? "Online" : "Offline"}</p>
-      <p>${result.country}</p>
-    `;
-    resultsContainer.appendChild(resultElement);
+      const resultElement = document.createElement("div");
+      resultElement.classList.add('result-con')
+      resultElement.style.backgroundImage = `url(${result.picture})`
+      resultElement.style.backgroundSize = 'cover';
+      resultElement.style.backgroundPosition = "center";
+
+      const resultDetails = document.createElement('div');
+      resultDetails.classList.add('result-details-con');
+
+      const nameElement = document.createElement('p');
+      nameElement.classList.add('result-name'); 
+      nameElement.textContent = result.name;
+
+      const infoCard = document.createElement('div');
+      infoCard.classList.add('info-card');
+      
+      const statusContainer = document.createElement('div');
+      statusContainer.classList.add('status-container');
+
+      // Create a status dot element and set its class based on online status
+      const statusDot = document.createElement('span');
+      statusDot.classList.add('status-dot', result.online ? 'online' : 'offline');
+
+      // Create a status text element to display "Online" or "Offline"
+      const statusText = document.createElement('span');
+      statusText.classList.add(result.online ? 'online-status' : 'offline-status');
+      statusText.textContent = result.online ? 'Online' : 'Offline';
+
+      // Append the status dot and status text to the status container
+      statusContainer.appendChild(statusDot);
+      statusContainer.appendChild(statusText);
+
+      const locationElement = document.createElement('p');
+      locationElement.classList.add('result-location'); 
+      locationElement.textContent = `${result.state}, ${result.country}`; 
+
+      
+      infoCard.appendChild(statusContainer);
+      infoCard.appendChild(locationElement);
+
+      resultDetails.appendChild(nameElement);  
+      resultDetails.appendChild(infoCard);
+
+      resultElement.appendChild(resultDetails);
+      resultsContainer.appendChild(resultElement);
   });
 };
 
-const handleSearch = (event) => {
-  event.preventDefault(); // Prevent form submission
+const handleCountrySearch = (event) => {
+  event.preventDefault();
 
   const query = searchInput.value.trim().toLowerCase();
+  const results = database.filter((item) =>
+      item.country.toLowerCase().includes(query)
+  );
 
-  // Make a request to your backend API to retrieve the search results
-  fetch(`/api/search?query=${query}`)
-    .then((response) => response.json())
-    .then((data) => {
-      displayResults(data.results);
-    })
-    .catch((error) => {
-      console.log(error);
-      displayResults([]);
-    });
+  displayResults(results);
 };
 
-document.querySelector(".search-form").addEventListener("submit", handleSearch);
+const handleStateSearch = (event) => {
+  event.preventDefault();
+
+  const query = searchInput.value.trim().toLowerCase();
+  const results = database.filter((item) =>
+      item.state.toLowerCase().includes(query)
+  );
+
+  displayResults(results);
+};
+
+document.querySelector(".search-form").addEventListener("submit", handleCountrySearch, handleStateSearch);
+
+
+
+window.onload = () => {
+  displayResults();
+}
